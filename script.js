83174c7ff406e0f9fdc3a4b3379e84f9f0799fe7
pixverseofficial <pixverse.official00@gmail.com>
2026-06-12 11:23:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
+const reveals=document.querySelectorAll(".reveal");
 
+window.addEventListener("scroll",()=>{
+
+reveals.forEach(item=>{
+
+const top=item.getBoundingClientRect().top;
+
+if(top<window.innerHeight-100){
+item.classList.add("active");
+}
+
+});
+
+});
+
+const counters=document.querySelectorAll(".count");
+
+counters.forEach(counter=>{
+
+const update=()=>{
+
+const target=+counter.getAttribute("data-target");
+const current=+counter.innerText;
+
+const increment=target/100;
+
+if(current<target){
+
+counter.innerText=Math.ceil(current+increment);
+
+setTimeout(update,20);
+
+}else{
+
+counter.innerText=target;
+
+}
+
+}
+
+update();
+
+});
